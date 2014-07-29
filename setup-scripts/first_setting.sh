0450478fb9e1b2d0c3fb8f2720d988adbc4ae70f
tarukosu <u_au_sky_future12@yahoo.co.jp>
2014-07-29 14:38:20 +0900

    add setup scripts

---
+++ setup-scripts/first_setting.sh
@@ -0,0 +1,7 @@
+LANG=C xdg-user-dirs-gtk-update
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get install gnome-session-fallback aptitude
+sudo apt-get install emacs gpointing-device-settings compizconfig-settings-manager terminator ibus-mozc apel migemo 
+sudo apt-get install rosemacs-el
+sudo apt-get install xautomation kazam
+
